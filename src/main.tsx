beb45746eb7eb2b2cd07209bcf738e0b5202fff2
Bhanaka <bhanaka94pramod@gmail.com>
2024-09-09 23:47:50 +0530

    learn about login part

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,15 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
 import App from './App';
-import store from './stores';
+// import store from './stores';
+import store from './stores_l';
 
 ReactDOM.render(
+  // <Provider store={store}>
+  //   <App />
+  // </Provider>,
   <Provider store={store}>
-    <App />
+    <h1>hello</h1>
   </Provider>,
   document.getElementById('root'),
 );
